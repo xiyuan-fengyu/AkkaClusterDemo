12a9895ad89b7fc31ae3d898c2d3ccbd1144f01b
xiyuan_fengyu <xiyuan_fengyu@163.com>
2016-07-18 11:17:06 +0800

    LinuxUtil工具类增加新方法 降低scala和akka的版本，使得可以在jdk1.7的环境运行 编写IDEA中的直接远程部署脚本

---
+++ src/main/scala/com/xiyuan/StopMaster.scala
@@ -0,0 +1,14 @@
+package com.xiyuan
+
+import com.xiyuan.util.LinuxUtil
+
+/**
+  * Created by xiyuan_fengyu on 2016/7/18 11:16.
+  */
+object StopMaster {
+
+  def main(args: Array[String]) {
+    LinuxUtil.killByPort(DeployMaster.masterPort, DeployMaster.masterHost, DeployMaster.masterUsername, DeployMaster.masterPassword)
+  }
+
+}
